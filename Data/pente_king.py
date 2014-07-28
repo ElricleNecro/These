3d05fb54b1852cd59450c4939cb4c45b4b2c5038
ElricleNecro <guiplum@gmail.com>
2014-07-28 17:21:03 +0200

    Augmentation de la taille des graphes.

--- Data/pente_king.py
+++ Data/pente_king.py
@@ -1,8 +1,12 @@
 # coding: utf-8
+
+from matplotlib import pyplot as plt
+import numpy as np
+
 get_ipython().system(" sed -e 's:None:0.0:g' pente-ci.dat > /tmp/toto.dat")
 data = np.loadtxt("/tmp/toto.dat")
 f = -10.0698 * np.exp( -0.220152 * data[:,0]) - 1.63409
-plt.figure(figsize=(3., 1.8))
+plt.figure(figsize=(4., 2.8))
 plt.ylabel(r"Pente $\alpha$")
 plt.xlabel("$W_0$")
 plt.plot(data[:,0], data[:,1], "+")
